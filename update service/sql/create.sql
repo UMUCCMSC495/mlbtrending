fd675c70440b595e0cbfef56b206a10750c44b05
Jonathan Kim <jkim8192@gmail.com>
2017-04-16 14:17:43 -0400

    Updated create table/view to create table if not exists/create or replace view

--- update service/sql/create.sql
+++ update service/sql/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE t_team (
+CREATE TABLE IF NOT EXISTS t_team (
     id INT NOT NULL AUTO_INCREMENT,
     abbr VARCHAR(8) NOT NULL,
     name VARCHAR(128) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE t_team (
     KEY (abbr)
 );
 
-CREATE TABLE t_game (
+CREATE TABLE IF NOT EXISTS t_game (
     away_id INT NOT NULL,
     home_id INT NOT NULL,
     game_date TIMESTAMP NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE t_game (
     FOREIGN KEY (away_id) REFERENCES t_team (id)
 );
 
-CREATE TABLE t_inning (
+CREATE TABLE IF NOT EXISTS t_inning (
     away_id INT NOT NULL,
     home_id INT NOT NULL,
     game_date TIMESTAMP NOT NULL,
@@ -38,87 +38,77 @@ CREATE TABLE t_inning (
     FOREIGN KEY (home_id, away_id, game_date) REFERENCES t_game (home_id, away_id, game_date)
 );
 
-CREATE VIEW
-    team
-AS
-    SELECT
-        abbr,
-        name,
-        city
-    FROM
-        t_team;
+CREATE OR REPLACE VIEW team AS
+SELECT
+    abbr,
+    name,
+    city
+FROM
+    t_team;
 
-CREATE VIEW
-    game
-AS
-    SELECT
-        away.abbr AS away_team,
-        home.abbr AS home_team,
-        game_date,
-        away.name AS away_name,
-        away.city AS away_city,
-        home.name AS home_name,
-        home.city AS home_city,
-        status,
-        location,
-        away_runs,
-        away_hits,
-        away_errors,
-        away_home_runs,
-        home_runs,
-        home_hits,
-        home_errors,
-        home_home_runs
-    FROM
-        t_game AS game
-            INNER JOIN t_team AS away ON game.away_id = away.id
-            INNER JOIN t_team AS home ON game.home_id = home.id;
+CREATE OR REPLACE VIEW game AS
+SELECT
+    away.abbr AS away_team,
+    home.abbr AS home_team,
+    game_date,
+    away.name AS away_name,
+    away.city AS away_city,
+    home.name AS home_name,
+    home.city AS home_city,
+    status,
+    location,
+    away_runs,
+    away_hits,
+    away_errors,
+    away_home_runs,
+    home_runs,
+    home_hits,
+    home_errors,
+    home_home_runs
+FROM
+    t_game AS game
+        INNER JOIN t_team AS away ON game.away_id = away.id
+        INNER JOIN t_team AS home ON game.home_id = home.id;
 
-CREATE VIEW
-    inning
-AS
-    SELECT
-        away.abbr AS away_team,
-        home.abbr AS home_team,
-        game_date,
-        inning_number,
-        away.name AS away_name,
-        away.city AS away_city,
-        home.name AS home_name,
-        home.city AS home_city,
-        away_runs,
-        home_runs
-    FROM
-        t_inning AS inning
-            INNER JOIN t_team AS away ON inning.away_id = away.id
-            INNER JOIN t_team AS home ON inning.home_id = home.id;
+CREATE OR REPLACE VIEW inning AS
+SELECT
+    away.abbr AS away_team,
+    home.abbr AS home_team,
+    game_date,
+    inning_number,
+    away.name AS away_name,
+    away.city AS away_city,
+    home.name AS home_name,
+    home.city AS home_city,
+    away_runs,
+    home_runs
+FROM
+    t_inning AS inning
+        INNER JOIN t_team AS away ON inning.away_id = away.id
+        INNER JOIN t_team AS home ON inning.home_id = home.id;
 
 /*
-CREATE VIEW
-    team_stats_by_month
-AS
-    SELECT
-        team.abbr AS abbr,
-        team.name AS name,
-        team.city AS city,
-        game_date,
-        away_runs,
-        away_home_runs
-    FROM
-        t_team AS team
+CREATE VIEW team_stats_by_month AS
+SELECT
+    team.abbr AS abbr,
+    team.name AS name,
+    team.city AS city,
+    game_date,
+    away_runs,
+    away_home_runs
+FROM
+    t_team AS team
 */
 
 /*
-CREATE VIEW
-    team_stats_by_year
-AS
-    SELECT
-        team.abbr AS abbr,
-        team.name AS name,
-        team.city AS city,
-        game_date,
-        away_runs,
-        away_home_runs
-    FROM
-        t_team AS team
+CREATE VIEW team_stats_by_year AS
+SELECT
+    team.abbr AS abbr,
+    team.name AS name,
+    team.city AS city,
+    game_date,
+    away_runs,
+    away_home_runs
+FROM
+    t_team AS team
 */
