c31214f6228aacf95d00b86517057f81c067c9fd
Jonathan Kim <jkim8192@gmail.com>
2017-04-16 19:53:17 -0400

    Created tests.py

---
+++ update service/tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestUpdateService(unittest.TestCase):
+    def testRetrieveData(self):
+        pass
+
+    def testGetDates(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main();
