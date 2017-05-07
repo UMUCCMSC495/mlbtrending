cb75e21a75fcf3aeee0a5ba7a5f7de12427a7c7f
Fred Long <fmlong37@gmail.com>
2017-05-07 15:13:32 -0400

    Adds displays in case of no data

--- mlbWeb/web-app/app/view/YesterdaysGames.js
+++ mlbWeb/web-app/app/view/YesterdaysGames.js
@@ -40,66 +40,78 @@ Ext.define('MlbApp.view.YesterdaysGames', {
         			
         			yesterdaysgamespanel.removeAll();
         			
-        			for(var i=0; i<games.length; i++)
+        			if(games.length<1)
         			{
         				var panel = new Ext.panel.Panel({
-        					title: me.renderTitle(games[i]),
-        					padding: '10 10 10 10'
+        		            html: '<p>&nbsp;&nbsp;&nbsp;No games played yesterday.</p>'
         				});
-        				
-        				var displayfield = new Ext.form.field.Display({
-        					anchor: '100%',
-        					renderer: function()
-        					{
-        						var innings = games[i].innings.length;
-        				    	var html = '<table border=0 cellpadding=15 cellspacing=0 width="100%">';
-        				    	
-        				    	/**
-        				    	 * Header: Start Rows
-        				    	 */
-        				    	var row1 = '<tr><th>&nbsp;</th>';
-        				    	var row2 = '<tr><td align="left"><b>' + games[i].away.abbr + '</b></td>';
-        				    	var row3 = '<tr><td align="left"><b>' + games[i].home.abbr + '</b></td>';
-        				    	
-        				    	//Header: dynamic number of innings
-            				    for (var j=0; j < games[i].innings.length; j++)
-        				    	{
-        				    		row1 += '<th>' + (parseInt(j) + 1) + '</th>';
-        				    		row2 += '<td align="center"><b>' + games[i].innings[j].awayRuns + '</b></td>';
-        				    		row3 += '<td align="center"><b>' + games[i].innings[j].homeRuns + '</b></td>';
-        				    	}
-                                
-                                // Ensure at least 9 innings
-                                if (games[i].innings.length < 9) {
-                                    for (var j=games[i].innings.length; j < 9; j++) {
-                                        row1 += '<th>' + (parseInt(j) + 1) + '</th>';
-                                    }
-                                    
-                                    var paddingColumns = 9 - games[i].innings.length;
-                                    
-                                    row2 += '<td colspan="' + paddingColumns + '"></td>';
-                                    row3 += '<td colspan="' + paddingColumns + '"></td>';
-                                }
-        				    	
-        				    	//Header: Runs, Errors, Homeruns
-        				    	row1 += '<th>R</th><th>E</th><th>HR</th></tr>';
-        				    	row2 += '<td><b>' + games[i].awayRuns + '</b></td><td><b>' + games[i].awayErrors + '</b></td><td><b>' + games[i].awayHomeRuns + '</b></td></tr>';
-        				    	row3 += '<td><b>' + games[i].homeRuns + '</b></td><td><b>' + games[i].homeErrors + '</b></td><td><b>' + games[i].homeHomeRuns + '</b></td></tr>';
-        				    	
-        				    	html += row1;
-        				    	html += row2;
-        				    	html += row3;
-        				    	html += '</table>';
-        				    	
-        				    	return html;
-        					}
-        				})
-        				
-        				panel.add(displayfield);
-        				panel.doLayout();
         				
         				var yesterdaysgamespanel = Ext.ComponentQuery.query('#yesterdaysgamespanel')[0];
         				yesterdaysgamespanel.add(panel);
+        			}
+        			else
+        			{
+	        			for(var i=0; i<games.length; i++)
+	        			{
+	        				var panel = new Ext.panel.Panel({
+	        					title: me.renderTitle(games[i]),
+	        					padding: '10 10 10 10'
+	        				});
+	        				
+	        				var displayfield = new Ext.form.field.Display({
+	        					anchor: '100%',
+	        					renderer: function()
+	        					{
+	        						var innings = games[i].innings.length;
+	        				    	var html = '<table border=0 cellpadding=15 cellspacing=0 width="100%">';
+	        				    	
+	        				    	/**
+	        				    	 * Header: Start Rows
+	        				    	 */
+	        				    	var row1 = '<tr><th>&nbsp;</th>';
+	        				    	var row2 = '<tr><td align="left"><b>' + games[i].away.abbr + '</b></td>';
+	        				    	var row3 = '<tr><td align="left"><b>' + games[i].home.abbr + '</b></td>';
+	        				    	
+	        				    	//Header: dynamic number of innings
+	            				    for (var j=0; j < games[i].innings.length; j++)
+	        				    	{
+	        				    		row1 += '<th>' + (parseInt(j) + 1) + '</th>';
+	        				    		row2 += '<td align="center"><b>' + games[i].innings[j].awayRuns + '</b></td>';
+	        				    		row3 += '<td align="center"><b>' + games[i].innings[j].homeRuns + '</b></td>';
+	        				    	}
+	                                
+	                                // Ensure at least 9 innings
+	                                if (games[i].innings.length < 9) {
+	                                    for (var j=games[i].innings.length; j < 9; j++) {
+	                                        row1 += '<th>' + (parseInt(j) + 1) + '</th>';
+	                                    }
+	                                    
+	                                    var paddingColumns = 9 - games[i].innings.length;
+	                                    
+	                                    row2 += '<td colspan="' + paddingColumns + '"></td>';
+	                                    row3 += '<td colspan="' + paddingColumns + '"></td>';
+	                                }
+	        				    	
+	        				    	//Header: Runs, Errors, Homeruns
+	        				    	row1 += '<th>R</th><th>E</th><th>HR</th></tr>';
+	        				    	row2 += '<td><b>' + games[i].awayRuns + '</b></td><td><b>' + games[i].awayErrors + '</b></td><td><b>' + games[i].awayHomeRuns + '</b></td></tr>';
+	        				    	row3 += '<td><b>' + games[i].homeRuns + '</b></td><td><b>' + games[i].homeErrors + '</b></td><td><b>' + games[i].homeHomeRuns + '</b></td></tr>';
+	        				    	
+	        				    	html += row1;
+	        				    	html += row2;
+	        				    	html += row3;
+	        				    	html += '</table>';
+	        				    	
+	        				    	return html;
+	        					}
+	        				})
+	        				
+	        				panel.add(displayfield);
+	        				panel.doLayout();
+	        				
+	        				var yesterdaysgamespanel = Ext.ComponentQuery.query('#yesterdaysgamespanel')[0];
+	        				yesterdaysgamespanel.add(panel);
+	        			}
         			}
         			yesterdaysgamespanel.doLayout();
         		},
