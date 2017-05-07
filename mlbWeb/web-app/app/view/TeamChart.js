c7a55b13470fdd1d6b3343d768aeadbe7ddc049d
Fred Long <fmlong37@gmail.com>
2017-05-06 20:20:33 -0400

    Stubs out fields for charting; need to backfill with real data

--- mlbWeb/web-app/app/view/TeamChart.js
+++ mlbWeb/web-app/app/view/TeamChart.js
@@ -3,34 +3,30 @@ Ext.define('MlbApp.view.TeamChart', {
 	    alias: 'widget.teamchart',
 	    xtype: 'basic-line',
 
-
 	    initComponent: function() {
 	        var me = this;
 
 	        this.myDataStore = Ext.create('Ext.data.JsonStore', {
-	            fields: ['month', 'data1' ],
+	            fields: ['month', 'data1', 'data2'],
 	            data: [
-	                { month: 'Jan', data1: 20 },
-	                { month: 'Feb', data1: 20 },
-	                { month: 'Mar', data1: 19 },
-	                { month: 'Apr', data1: 18 },
-	                { month: 'May', data1: 18 },
-	                { month: 'Jun', data1: 17 },
-	                { month: 'Jul', data1: 16 },
-	                { month: 'Aug', data1: 16 },
-	                { month: 'Sep', data1: 16 },
-	                { month: 'Oct', data1: 16 },
-	                { month: 'Nov', data1: 15 },
-	                { month: 'Dec', data1: 15 }
+	                { month: 'Jan', data1: 20, data2: 19 },
+	                { month: 'Feb', data1: 20, data2: 18 },
+	                { month: 'Mar', data1: 19, data2: 16 },
+	                { month: 'Apr', data1: 18, data2: 14 },
+	                { month: 'May', data1: 18, data2: 15 }
 	            ]
 	        });
 
 
 	        me.items = [{
 	            xtype: 'chart',
+	            legend: {
+	                position: 'right'
+	            },
 	            height: 410,
 	            width: 780,
 	            padding: '10 0 0 0',
+	            
 	            style: {
 	                'background' : '#fff'
 	            },
@@ -61,13 +57,14 @@ Ext.define('MlbApp.view.TeamChart', {
 	            }],
 	            axes: [{
 	                type: 'numeric',
-	                fields: 'data1',
+	                fields: ['data1','data2'],
 	                position: 'left',
 	                grid: true,
 	                minimum: 0,
 	                label: {
 	                    renderer: function(v) { return v + '%'; }
-	                }
+	                },
+	                title: 'Test Title'
 	            }, {
 	                type: 'category',
 	                fields: 'month',
@@ -77,7 +74,8 @@ Ext.define('MlbApp.view.TeamChart', {
 	                    rotate: {
 	                        degrees: -45
 	                    }
-	                }
+	                },
+	                title: 'Bottom Axis Title'
 	            }],
 	            series: [{
 	                type: 'line',
@@ -107,6 +105,35 @@ Ext.define('MlbApp.view.TeamChart', {
 	                        this.setTitle(storeItem.get('month') + ': ' + storeItem.get('data1') + '%');
 	                    }
 	                }
+	            },{
+	                type: 'line',
+	                axis: 'left',
+	                xField: 'month',
+	                yField: 'data2',
+	                style: {
+	                    'stroke-width': 4
+	                },
+	                markerConfig: {
+	                    radius: 4
+	                },
+	                highlight: {
+	                    fill: '#000',
+	                    radius: 5,
+	                    'stroke-width': 2,
+	                    stroke: '#fff'
+	                },
+	                tips: {
+	                    trackMouse: true,
+	                    style: 'background: #FFF',
+	                    height: 20,
+	                    showDelay: 0,
+	                    dismissDelay: 0,
+	                    hideDelay: 0,
+	                    renderer: function(storeItem, item) {
+	                        this.setTitle(storeItem.get('month') + ': ' + storeItem.get('data1') + '%');
+	                    }
+	                }
+	            	
 	            }]
 	        }];
 
